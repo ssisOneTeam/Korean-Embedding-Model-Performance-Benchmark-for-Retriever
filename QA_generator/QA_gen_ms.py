c3ebe8f62bdb8799444a5f1e4231466715446fb2
Noveled <noveled@naver.com>
2023-12-14 15:04:39 +0900

    1214 민식 수정

--- QA_generator/QA_gen_ms.py
+++ QA_generator/QA_gen_ms.py
@@ -1,7 +1,12 @@
 import os
+import re
 import tqdm
 from openai import OpenAI
 import pandas as pd
+
+## 설정
+csv_file_name = './QA_ch_03.csv'
+txt_file_name = "backup_ch_03.txt" # 텍스트를 저장할 파일명
 
 llm = {
     'gpt-3.5':"gpt-3.5-turbo",
@@ -9,8 +14,9 @@ llm = {
 }
 
 client = OpenAI(
-    organization=os.getenv("OPENAI_ORGARNIZATION_KEY"), # 환경변수에 OPENAI_ORGANIZATION_KEY를 설정합니다.
+    organization=os.getenv("your_organization_id"), # 환경변수에 OPENAI_ORGANIZATION_KEY를 설정합니다.
     api_key=os.getenv("OPENAI_API_KEY") # 환경변수에 OPENAI_API_KEY를 설정합니다.
+    # api_key="your api key" # 환경변수에 OPENAI_API_KEY를 설정합니다.
 )
 
 # Config
@@ -61,14 +67,14 @@ def saveCSV(data):
     answers = []
     documents = []
 
-    # 데이터를 순회하며 Q, A, Doc에 해당하는 내용을 각 리스트에 추가
-    for item in data:
-        if item.startswith('(Q'):
-            questions.append(item[5:])
-        elif item.startswith('(A'):
-            answers.append(item[5:])
-        elif item.startswith('(Doc)'):
-            documents.append(item[6:])
+    if len(data) % 3:
+        print("뭔가 잘못 됨!!")
+        return
+    
+    for i in range(0, len(data), 3):
+        questions.append(data[i])
+        answers.append(data[i+1])
+        documents.append(data[i+2])
 
     # 데이터 프레임 생성
     df = pd.DataFrame({
@@ -81,14 +87,19 @@ def saveCSV(data):
     return df
 
 if __name__ == "__main__":
-    ABS_DIR = "../embeddingtest/data/test/" # 최상위 디렉터리 경로
+    print("현재 경로 : ",os.getcwd())
+    ABS_DIR = "../embeddingtest/data/test/" #질문셋 만들 대상 챕터 넣는 디렉터리 경로
     df = pd.DataFrame(columns=['Question', 'Answer', 'Documents'])
     chapter_list = os.listdir(ABS_DIR)
     outlist = ["readme.md", ".DS_Store"]
     md_contents = []
     file_list = []
     Gen_data = []
+    items = []
 
+    # csv 파일 생성
+    df.to_csv('./QA_ch_03.csv', mode='w', encoding='utf-8', index=False)
+    
     # 디렉토리에서 각 Chapter에 해당하는 MD파일과 목록 파싱
     for title in chapter_list :
         # 예외처리
@@ -99,7 +110,7 @@ if __name__ == "__main__":
         for root, dirs, files in os.walk(absolute_path):
             for file in files:
                 if file.endswith(".md"):
-                    with open(os.path.join(root, file), 'r') as f:
+                    with open(os.path.join(root, file), 'r',encoding='utf-8') as f:
                         file_list.append(file)
                         md_contents.append(f.read())
     
@@ -108,10 +119,11 @@ if __name__ == "__main__":
     # ======================================================================
     #       Generate QA dataset
     # ======================================================================
-    for contents in md_contents: 
-    # for contents in md_contents[:1]: # Test용 1개 제도
+    for contents in md_contents: # ABS 폴더에 들어있는 모든 제도에 대해서 질문 만들어야됨 
+
         content = list(contents.split('\n\n'))
         # Log
+        print("=="*50)
         print("    Content : ", content)
         print("=="*50)
 
@@ -123,9 +135,9 @@ if __name__ == "__main__":
 
             위 내용을 반영한 한국어 질문-응답-문맥 으로 이루어진 데이터셋 샘플 3개를 생성해줘.
             내용은 절대 중복되면 안돼. 질문앞에는 (Q)가 붙고, 응답 앞에는 (A)가 붙어. 
-            만들어진 응답 뒤에 한줄 띄고 무조건 아래 내용을 이어붙여줘. 제도명은 위 내용의 제목을 사용하면 돼.
+            만들어진 응답 뒤에 한줄 띄고 무조건 아래 내용을 이어붙여줘. 
 
-            '(Doc) 제도명'
+            '(Doc) {content[0]}'
             """
 
             qa_gen_long = f"""
@@ -133,11 +145,11 @@ if __name__ == "__main__":
             {paragraph}
             ---
 
-            위 내용을 반영한 한국어 질문-응답-문맥 으로 이루어진 데이터셋 샘플 10개를 생성해줘.
+            위 내용을 반영한 한국어 질문-응답-문맥 으로 이루어진 데이터셋 샘플 6개를 생성해줘.
             내용은 절대 중복되면 안돼. 질문앞에는 (Q)가 붙고, 응답 앞에는 (A)가 붙어. 
-            만들어진 응답 뒤에 한줄 띄고 무조건 아래 내용을 이어붙여줘. 제도명은 위 내용의 제목을 사용하면 돼.
+            만들어진 응답 뒤에 한줄 띄고 무조건 아래 내용을 이어붙여줘. 
 
-            '(Doc) 제도명'
+            '(Doc) {content[0]}'
             """
 
             qa_dataset = []
@@ -151,13 +163,43 @@ if __name__ == "__main__":
             
             qa_dataset.append(res.replace('\n\n', '\n'))
             
-            temp_data = []
-            temp_data.append(res)
-            print(" > 데이터셋 생성중... ", qa_dataset)
+            print("\n\n > 데이터셋 생성중... ", qa_dataset)
 
             for item in qa_dataset:
-                Gen_data.extend(list(item.split('\n')))
+                # Gen_data.extend(list(item.split('\n')))
+                
+                # 재준이형 바꿔본 부분
+                ## (Q), (A), (Doc) 부분도 같이 없애려고 해본건데
+                ## saveCSV 부분도 바꿔야 되서 일단 다시 생각중
+                ### 수정하여 돌림
+                Gen_data.extend([s.strip() for s in re.split(pattern='\([QA]\d*\)|\(Doc\)', string=item) if s])
+                ## 백업용
+                items.append(item)
 
-    print("Results : ", Gen_data)
-    OurData = saveCSV(Gen_data)
-    OurData.to_csv('./OurData.csv')
+            #
+            # 데이터 저장 파트
+            # 
+
+            ## split전 내용 저장 -> 혹시 잘못됐을시 체크하고 대안세우기 위한 백업
+            backup = "\n\n".join(items)
+
+            # 저장할 텍스트 내용
+            text_to_save = backup
+
+            # 파일 열기 (읽기쓰기 모드)
+            with open(txt_file_name, 'a+') as txtfile:
+                # 텍스트 쓰기
+                txtfile.write(text_to_save + '\n')
+
+            print(f'Text "{text_to_save}" has been saved to {txt_file_name}')
+            
+            print("Results : ", Gen_data)
+
+            OurData = saveCSV(Gen_data)
+            OurData.to_csv(csv_file_name, mode='a+', encoding='utf-8', index=False, header=False)
+            # OurData.to_csv('./QA_ch_03.csv', encoding='utf-8') #chapter바뀌면 저장csv명 도 바꿔줘   './QA_ch_xx.csv'
+            print("*** csv 생성이 완료 되었습니다 ***") 
+
+            # 리스트 초기화 
+            Gen_data = []
+            items = []
